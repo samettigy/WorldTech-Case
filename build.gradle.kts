a88ef34c4df8e8c9fb040ed1bad59c789ed25c66
samettigy <116752451+samettigy@users.noreply.github.com>
2024-07-10 17:45:25 +0300

    Finish

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ buildscript {
 
 plugins {
     id("com.google.gms.google-services") version "4.4.0" apply false
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android")  version "2.50" apply false
 }
